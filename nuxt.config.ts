854119dc71728efb600f47548690b4018a733863
Jack Barham <>
2025-06-15 12:45:41 +0100

    App content for SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,5 +29,26 @@ export default defineNuxtConfig({
         display: 'swap'
       },
     ]
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      title: 'Nuxt starter template',
+      // meta: [
+      //   { 
+      //     name: 'description', 
+      //     content: 'Web development, interface design and digital consultancy.'
+      //   }
+      // ],
+      // link: [
+      //   { 
+      //     rel: 'shortcut icon', 
+      //     type: 'image/png', 
+      //     href: 'https://a.storyblok.com/f/299693/130x128/f7e6b5d5e7/favicon.png?v=1'
+      //   },
+      // ],
+    }
   }
 })
