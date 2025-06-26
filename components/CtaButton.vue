8e1e08af4cb655ff5a029239cb4e4006c60a2d05
Jack Barham <>
2025-06-26 11:42:48 +0100

    Button props

--- components/CtaButton.vue
+++ components/CtaButton.vue
@@ -1,13 +1,32 @@
 <template>
   <NuxtLink
-    :to="somePath"
+    :to="props.link"
     class="inline-block w-full bg-brand-red text-white px-6 py-3 rounded-full hover:bg-brand-red-dark transition-colors"
-    :aria-label="label"
+    :aria-label="props.text"
   >
-    <span class="block text-xl font-medium tracking-wide text-center">Button text</span>
+    <span class="block text-xl font-medium tracking-wide text-center">{{ props.text }}</span>
   </NuxtLink>
 </template>
 
 <script setup>
-//  
+const props = defineProps({
+  link: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  // theme: {
+  //   type: String,
+  //   required: false,
+  //   default: 'dark',
+  // },
+  // arrow: {
+  //   type: Boolean,
+  //   required: false,
+  //   default: true,
+  // },
+})
 </script>
